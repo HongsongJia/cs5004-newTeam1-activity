68049688a7d82b28957806959fb709b45077df7d
yihongkun <yihongkun513@gmail.com>
2026-01-29 22:38:18 -0800

    Format HongkunTest.java for consistency

--- module03/HongkunTest.java
+++ module03/HongkunTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HongkunTest {
-
+ 
     @Test
     void testFront11_Basic() {
         Hongkun solver = new Hongkun();
