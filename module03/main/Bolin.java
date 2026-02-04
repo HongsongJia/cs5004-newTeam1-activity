e358b9750e9ace9657dd77b5ad16644bd78553e5
1592107333 <z1592107333@gmail.com>
2026-02-04 03:42:55 -0800

    edit documentation

--- module03/main/Bolin.java
+++ module03/main/Bolin.java
@@ -2,7 +2,29 @@
 
 /**
  * @author Bolin Zeng
- * Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given target? This is a classic backtracking recursion problem. Once you understand the recursive backtracking strategy in this problem, you can use the same pattern for many problems to search a space of choices. Rather than looking at the whole array, our convention is to consider the part of the array starting at index start and continuing to the end of the array. The caller can specify the whole array simply by passing start as 0. No loops are needed -- the recursive calls progress down the array.
+ * Date: 2/4, 2026
+ * Course: CS 5004
+ * Team: Team 2
+ * Module: 03
+ *
+ * Practice Problem: Recursive Factorial Calculation
+ *
+ * Problem Description:
+ * Given an array of ints, is it possible to choose a group of some of the ints,
+ * such that the group sums to the given target? This is a classic backtracking
+ * recursion problem. Once you understand the recursive backtracking strategy in
+ * this problem, you can use the same pattern for many problems to search a space
+ * of choices. Rather than looking at the whole array, our convention is to consider
+ * the part of the array starting at index start and continuing to the end of the array.
+ * The caller can specify the whole array simply by passing start as 0. No loops
+ * are needed -- the recursive calls progress down the array.
+ *
+ * Examples:
+ * groupSum(0, [2, 4, 8], 10) → true
+ * groupSum(0, [2, 4, 8], 14) → true
+ * groupSum(0, [2, 4, 8], 9) → false
+ *
+ * Source: [CodingBat, recursion]
  */
 public class Bolin{
 
