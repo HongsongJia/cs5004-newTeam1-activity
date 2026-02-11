17766465b1b6d8b6a8e5b7256bc784e0c3d3ab25
Zephyr Fang <zephyr1483@gmail.com>
2026-02-11 14:42:15 -0800

    Update YunmengzeFangTest.java

--- module05/test/YunmengzeFangTest.java
+++ module05/test/YunmengzeFangTest.java
@@ -1,27 +1,38 @@
 /**
- * Test class for YunmengzeFang countCode method.
- * Demonstrates simple unit-style testing using main.
+ * Name: Yunmengze Fang
+ * Course: CS5004
+ * Date: February 10, 2026
+ *
+ * Test class for centeredAverage method.
  */
 
 public class YunmengzeFangTest {
 
     public static void main(String[] args) {
 
-        YunmengzeFang z = new YunmengzeFang();
+        YunmengzeFang y = new YunmengzeFang();
 
-        // Test case 1
-        String test1 = "aaacodebbb";
-        int result1 = z.countCode(test1);
-        System.out.println("Test 1: " + result1);
+        // Test Case 1
+        int[] test1 = {1, 2, 3, 4, 100};
+        int result1 = y.centeredAverage(test1);
+        System.out.println("Test 1 Input: [1, 2, 3, 4, 100]");
+        System.out.println("Expected: 3");
+        System.out.println("Actual: " + result1);
+        System.out.println();
 
-        // Test case 2
-        String test2 = "codexxcode";
-        int result2 = z.countCode(test2);
-        System.out.println("Test 2: " + result2);
+        // Test Case 2
+        int[] test2 = {1, 1, 5, 5, 10, 8, 7};
+        int result2 = y.centeredAverage(test2);
+        System.out.println("Test 2 Input: [1, 1, 5, 5, 10, 8, 7]");
+        System.out.println("Expected: 5");
+        System.out.println("Actual: " + result2);
+        System.out.println();
 
-        // Test case 3
-        String test3 = "cozexxcope";
-        int result3 = z.countCode(test3);
-        System.out.println("Test 3: " + result3);
+        // Test Case 3
+        int[] test3 = {-10, -4, -2, -4, -2, 0};
+        int result3 = y.centeredAverage(test3);
+        System.out.println("Test 3 Input: [-10, -4, -2, -4, -2, 0]");
+        System.out.println("Expected: -3");
+        System.out.println("Actual: " + result3);
     }
 }
