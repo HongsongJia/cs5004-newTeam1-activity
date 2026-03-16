7e4866fe3bbfbe529bcdb15fdbe3617a25392754
bo0Zeng <bolinzeng@qq.com>
2026-03-16 12:28:36 -0700

    code

---
+++ module09/main/java/Code/student/CalculatorApp.java
@@ -0,0 +1,29 @@
+package student;
+
+
+import student.controller.CalculatorController;
+import student.controller.IController;
+import student.model.Calculator;
+import student.model.ICalculator;
+import student.view.ConsoleView;
+import student.view.IView;
+import student.view.JFrameView;
+
+public class CalculatorApp {
+
+    public static void main(String[] args) {
+
+        ICalculator model = new Calculator();
+        IController controller = new CalculatorController(model);
+        IView view;
+        if (args.length > 0 && args[0].equals("console")) {
+            view = new ConsoleView(controller);
+        } else {
+            view = new JFrameView(controller);
+        }
+
+
+        view.start();
+
+    }
+}
---
+++ module09/main/java/Code/student/view/JFrameView.java
@@ -0,0 +1,96 @@
+package student.view;
+
+
+import javax.swing.JFrame;
+import student.Settings;
+import student.controller.IController;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+
+public class JFrameView extends JFrame implements IView {
+
+
+    private static final Settings settings = Settings.getInstance();
+    private IController controller;
+    CalcDisplay calcDisplay;
+
+    public JFrameView(IController controller) {
+        super(settings.CAPTION);
+        this.controller = controller;
+        setLocationRelativeTo(null); // center the window
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close the application when the window is
+                                                        // closed
+
+        // set the display
+        calcDisplay = new CalcDisplay();
+        this.add(calcDisplay, BorderLayout.NORTH);
+
+        // set the buttons plane
+        CalcNumbers calcNumbers = new CalcNumbers();
+        calcNumbers.setNumberButtonListener(this::calcNumbersListener);
+        calcNumbers.setClearButtonListener(this::clearListener);
+
+        this.add(calcNumbers, BorderLayout.WEST);
+
+
+
+        // set the operations plane
+        CalcOperations calcOperations = new CalcOperations();
+        calcOperations.setOperationButtonListener(this::operationListener);
+        calcOperations.setEqualsButtonListener(this::equalsListener);
+
+        this.add(calcOperations);
+
+
+        pack(); // ensures frame is large enough to display all components, but not larger than
+                // necessary
+        calcDisplay.clear(); // make sure the display is clear at the beginning
+    }
+
+
+    private void calcNumbersListener(ActionEvent e) {
+        JButton clickedButton = (JButton) e.getSource();
+        String buttonLabel = clickedButton.getText();
+        if (calcDisplay.getText().equals("0") && !buttonLabel.equals(".")) {
+            calcDisplay.updateDisplay(buttonLabel, false);
+        } else {
+            calcDisplay.updateDisplay(buttonLabel);
+        }
+
+
+    }
+
+    private void clearListener(ActionEvent e) {
+        calcDisplay.clear();
+    }
+
+    private void operationListener(ActionEvent e) {
+        JButton clickedButton = (JButton) e.getSource();
+        String operationSymbol = clickedButton.getText();
+        calcDisplay.updateDisplay(" " + operationSymbol + " ");
+    }
+
+    private void equalsListener(ActionEvent e) {
+        try {
+            Number result = controller.processOperation(calcDisplay.getText());
+            calcDisplay.updateDisplay(result.toString(), false);
+        } catch (Exception ex) {
+            if (ex.getMessage() != null) {
+                calcDisplay.updateDisplay(ex.getMessage(), false);
+            } else {
+                calcDisplay.updateDisplay("Error", false);
+            }
+        }
+    }
+
+
+
+    @Override
+    public void start() {
+        setVisible(true);
+    }
+
+
+
+}
